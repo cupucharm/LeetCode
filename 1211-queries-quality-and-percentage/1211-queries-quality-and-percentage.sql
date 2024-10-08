7fba89c21bfddfc295d2ab3b36ff6ed75e664bf6
Choi Sujin <43837946+cupucharm@users.noreply.github.com>
2024-10-08 19:52:48 +0900

    Time: 364 ms (42.33%), Space: 0B (100%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -3,4 +3,5 @@ SELECT query_name,
     ROUND(SUM(rating / position) / COUNT(*), 2) AS quality, 
     ROUND((SUM(rating < 3) / COUNT(*)) * 100, 2) AS poor_query_percentage
 FROM Queries
+WHERE query_name is not null
 GROUP BY query_name;
